8f7176f67929af13594f7ead7dde4db0f0679a22
DESKTOP-R4GODQ9\tom <tom@schreckmail.com>
2020-09-15 09:20:20 -0500

    fix(mapper): refine mapper to use Result for toDomain only

--- src/infra/mapper.ts
+++ src/infra/mapper.ts
@@ -4,13 +4,6 @@ import { Result } from "domain/result";
 export interface Mapper<TDomain, TDto>
 {
   toDTO(input: TDomain): TDto;
-  toDomain(input: TDto): TDomain;
-  // toPersistence (t: T): any;
-}
-
-export interface MapperResults<TDomain, TDto>
-{
-  toDTO(input: TDomain): Result<TDto>;
   toDomain(input: TDto): Result<TDomain>;
   // toPersistence (t: T): any;
 }
